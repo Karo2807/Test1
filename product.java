63f254dfd40d4730ed9ef5b086459d7682757c73
Karo <karo_cm@gmx.at>
2019-03-18 14:14:24 +0100

    Product name

---
+++ product.java
@@ -0,0 +1,22 @@
+
+public class Product
+{       private String name ("Coca Cola");
+
+        public Product(String nameCons){
+
+                this.name = nameCons;
+        }
+
+        public void SetName(String value){
+
+        this.name = value;
+        }
+
+        public String GetName(){
+
+        return this.name;
+        }
+}
+~
+
+
